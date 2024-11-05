b385b112cd76c7d77c998f498879220e7406aed6
ansira-github <fathimath.23cs038@sode-edu.in>
2024-11-05 15:38:06 +0530

    Add files via upload

---
+++ F6.java
@@ -0,0 +1,5 @@
+
+public class F6 {
+    public static void main(String[] args) {
+		Tools t1=new Tools();
+		System.out.println(t1.reverse("Ansira"));}}
---
+++ F8.java
@@ -0,0 +1,10 @@
+
+public class F8{
+public static void main(String[] args) {
+		Tools t1=new Tools();
+		System.out.println(t1.reverse("Ansira"));
+		
+
+	}
+
+}
