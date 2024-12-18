43051e2bd6a86f65e4e3e5fb778b631dd9929379
ansira-github <fathimath.23cs038@sode-edu.in>
2024-12-18 11:18:25 +0530

    Add files via upload

--- test1.java
+++ test1.java
@@ -1,24 +1,18 @@
+
+class MyPackage{
+	public void display() {
+		System.out.println("This msg is from MyPackage class within pkg2");
+	}
+}
 public class test1 {
-   public static void main(String[] args){ 
-       System.out.println("MIN and MAX Values:");
-       System.out.println("Byte MIN:"+Byte.MIN_VALUE+",MAX:"+Byte.MAX_VALUE);
-       System.out.println("Short MIN:"+Short.MIN_VALUE+",MAX:"+Short.MAX_VALUE);
-      System.out.println("Int MIN:"+Integer.MIN_VALUE+",MAX:"+Integer.MAX_VALUE);
-      System.out.println("Long MIN:"+Long.MIN_VALUE+",MAX:"+Long.MAX_VALUE); 
-      System.out.println("Float MIN:"+Float.MIN_VALUE+",MAX:"+Float.MAX_VALUE);  
-      System.out.println("Double MIN:"+Double.MIN_VALUE+",MAX:"+Double.MAX_VALUE); 
-      System.out.println("\nDefault Values(Initialized manually since primitives don't have defaults outside classes):");
-      byte defaultByte = 0;
-      short defaultShort = 0;
-      int defaultInt = 0;
-      long defaultLong = 0L;
-      float defaultFloat = 0.0f;
-      double defaultDouble = 0.0d;
-      System.out.println("Default byte:"+defaultByte);
-     System.out.println("Default short:"+defaultShort);
-     System.out.println("Default int:"+defaultInt);
-     System.out.println("Default long:"+defaultLong);
-     System.out.println("Default float:"+defaultFloat);
-     System.out.println("Default double:"+defaultDouble);
-   }   
+
+	public static void main(String[] args) {
+		System.out.println("Package");
+		MyPackage mypack1=new MyPackage();
+		MyPackage mypack2=new MyPackage();
+		MyPackage mypack3=new MyPackage();
+		mypack1.display();
+		mypack2.display();
+		mypack3.display();
+	}
 }
